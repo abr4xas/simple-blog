7da4d8930eeace3ee281562d72bbf014eb942325
angel cruz <angel.c@geopagos.com>
2020-12-26 15:18:27 -0300

    I CAN HAZ COMMENTZ.

--- src/SimpleBlogServiceProvider.php
+++ src/SimpleBlogServiceProvider.php
@@ -18,11 +18,18 @@ class SimpleBlogServiceProvider extends ServiceProvider
                 __DIR__ . '/../resources/views' => base_path('resources/views/vendor/simple-blog'),
             ], 'views');
 
-            $migrationFileName = 'create_simple_blog_table.php';
-            if (! $this->migrationFileExists($migrationFileName)) {
-                $this->publishes([
-                    __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
-                ], 'migrations');
+            $migrationFileNames = [
+                'create_articles_table.php',
+                'create_categories_table.php',
+                'add_category_id_to_articles_table.php',
+            ];
+
+            foreach ($migrationFileNames as $key => $value) {
+                if (! $this->migrationFileExists($key)) {
+                    $this->publishes([
+                        __DIR__ . "/../database/migrations/{$key}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $key),
+                    ], 'migrations');
+                }
             }
 
             $this->commands([
